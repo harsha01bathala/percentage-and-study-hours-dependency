e1ed5831b26bb23e2091799d5c5f127abb7d8432
harsha01bathala <40991148+harsha01bathala@users.noreply.github.com>
2020-07-16 00:57:41 +0530

    Add files via upload
    Added test cases like No.of Hours shouldn't exceed 24 hours and percentage should not cross 100

---
+++ flask deploy/app1.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 15 23:27:40 2020
+
+@author: Harsha
+"""
+
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = prediction[0]
+    if (output > 100):
+        output = 100
+    if (int_features[0] > 24):
+        return render_template('index.html', prediction_text='Enter a value less than 24')
+    else:
+        return render_template('index.html', prediction_text='expected percentage for ${} hours study$ {}'.format(int_features,output))
+        
+    
+
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    '''
+    For direct API calls trought request
+    '''
+    data = request.get_json(force=True)
+    prediction = model.predict([np.array(list(data.values()))])
+
+    output = prediction[0]
+    return jsonify(output)
+
+if __name__ == "__main__":
+    app.run(debug=True)
